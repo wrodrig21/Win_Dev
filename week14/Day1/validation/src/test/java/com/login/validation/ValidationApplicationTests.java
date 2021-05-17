b677bc648248241c23a25195e663e3b2bca0786c
wrodrig21 <79155084+wrodrig21@users.noreply.github.com>
2021-05-17 16:11:18 -0700

    hw commit

---
+++ week14/Day1/validation/src/test/java/com/login/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
